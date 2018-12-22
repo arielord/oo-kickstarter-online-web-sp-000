6f81329358d66db48f2be1195395b8b7bdc368b3
Ariel <arielord@buffalo.edu>
2018-12-22 04:28:31 +0000

    Automatically backed up by Learn

--- lib/backer.rb
+++ lib/backer.rb
@@ -1,11 +1,13 @@
 class Backer
   attr_accessor :name
+  attr_reader :projects
   
   def initialize(name)
     self.name = name
   end
   
   def back_project(project)
-    
+    @projects << project
+    self.projects
   end
 end
